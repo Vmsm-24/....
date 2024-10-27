1c6364f0801ed2964d6be5ee0f4fb99eb7a7997c
Vmsm-24 <suarezvaleria526@gmail.com>
2024-10-26 21:36:16 -0500

    o

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Mascotas perro = new Mascotas("Salchicha", "Milo", "Negro", 4);
+        /*perro.setRaza("Salchicha");
+        perro.setNombre("Milo");*/
+        //perro.setColor("Negro");
+        //perro.setEdad(4);
+        System.out.println("perro.detallemasc() = " + perro.detallemasc());
+        Mascotas gato = new Mascotas("Siames", "Joaquin", "Cafe", 9);
+        //gato.setColor("Cafe");
+        //gato.setEdad(9);
+        System.out.println("gato.detallemasc() = " + gato.detallemasc());
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
 }
--- src/Mascotas.java
+++ src/Mascotas.java
@@ -1,4 +1,79 @@
-package PACKAGE_NAME;
-
 public class Mascotas {
+    //Atributos o Estados
+    private String raza;
+    private String nombre;
+    private String color;
+    private float edad;
+
+    //Constructores y Destructores
+    public Mascotas(){
+
+    }
+
+    public Mascotas(String raza) {
+        this.raza = raza;
+    }
+
+    public Mascotas(String raza, String nombre) {
+        this.raza = raza;
+        this.nombre = nombre;
+    }
+
+    public Mascotas(String raza, String nombre, String color) {
+        this.raza = raza;
+        this.nombre = nombre;
+        this.color = color;
+    }
+
+    public Mascotas(String raza, String nombre, String color, float edad) {
+        this.raza = raza;
+        this.nombre = nombre;
+        this.color = color;
+        this.edad = edad;
+    }
+
+    //Métodos propios del lenguaje Java
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public float getEdad() {
+        return edad;
+    }
+
+    public void setEdad(float edad) {
+        this.edad = edad;
+    }
+
+    //Métodos propios de programador
+    public String detallemasc(){
+        String masc = "La raza es -> " + this.raza + "\n" +
+                "El nombre es -> " + this.nombre + "\n" +
+                "El color es -> " + this.color +"\n" +
+                "La edad es -> " + this.edad;
+        return masc;
+
+    }
+
+
 }
